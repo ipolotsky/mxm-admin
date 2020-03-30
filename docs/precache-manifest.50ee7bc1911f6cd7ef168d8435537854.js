2742b1ccdcdb561e24ca60aff69cb14fbede9a2b
Ilya Polotsky <ipolo.box@gmail.com>
2020-03-30 05:44:12 +0300

    Add build

---
+++ docs/precache-manifest.50ee7bc1911f6cd7ef168d8435537854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc7ee9da628abfb8cbb770a5c88e796",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b66465144e00158ed1",
+    "url": "/static/css/main.7053b39b.chunk.css"
+  },
+  {
+    "revision": "212f35f9c13b16224280",
+    "url": "/static/js/2.30c6002e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30c6002e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b66465144e00158ed1",
+    "url": "/static/js/main.3a9b4173.chunk.js"
+  },
+  {
+    "revision": "616aca1e092f2dc764cd",
+    "url": "/static/js/runtime-main.ba8ff7fc.js"
+  },
+  {
+    "revision": "78baeaab7d371bafba36fe10772cd6a1",
+    "url": "/static/media/image.78baeaab.png"
+  }
+]);
